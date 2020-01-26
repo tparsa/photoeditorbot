852cb7eaffa661f96f0c3492124b94535d9a1986
Parsa <parsa_ghorbani@outlook.com>
2020-01-27 03:16:27 +0330

    WIP: fix bugs

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import asyncio
 import telepot
 from telepot.aio.loop import MessageLoop
 from telepot.aio.delegate import pave_event_space, per_chat_id, create_open
-import PIL
+from PIL import Image
 
 class MessageCounter(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
@@ -18,29 +18,37 @@ class PhotoEditor(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(PhotoEditor, self).__init__(*args, **kwargs)
 
-    def edit(image_filename):
+    def edit(self, image_filename):
             try:
-                image = PIL.Image.open(image_filename)
+                image = Image.open(image_filename + '.png')
                 # GrayScale
-                img_array = np.array(image + '.png')
+                print("Start GrayScaling")
+                import numpy as np
+                img_array = np.array(image)
+                print(img_array)
                 for row in len(img_array):
                     for pixel in row:
                         grayscale = (0.3 * pixel[0]) + (0.59 * pixel[1]) + (0.11 * pixel[2])
                         pixel = [grayscale, grayscale, grayscale]
+                print("Done GrayScaling Start Saving")
 
-                image = PIL.Image.fromarray(img_array, 'RGB')
+                image = Image.fromarray(img_array, 'RGB')
                 img.save(image_filename + 'edited.png')
             except Exception as e:
+                print("FUCK")
                 print(e)
 
     async def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         
         if content_type == 'photo':
-            bot.download_file(msg['photo'][-1]['file_id'], './' + str(chat_id) + '.png')
+            print("Yo")
+            await bot.download_file(msg['photo'][-1]['file_id'], './' + str(chat_id) + '.png')
+            print("dafuq")
             self.edit(str(chat_id))
-            bot.sendPhoto(chat_id, open(str(chat_id) + 'edited.png'))
+            await bot.sendPhoto(chat_id, open(str(chat_id) + 'edited.png'))
         else:
+            print(content_type)
             bot.sendMessage(chat_id, 'Yo')
 
 TOKEN = sys.argv[1]  # get token from command-line
